3f2ed447f4a2af7ac180046964a9a7f6bb8ab124
yuding <453594138@qq.com>
2019-10-15 17:38:01 +0800

    java new files
    9-1~9-5

---
+++ 9-1/src/TestCircle.java
@@ -0,0 +1,48 @@
+
+public class TestCircle {
+    public static void main(String[] args){
+        //create a circle with radius 1
+        Circle circle1=new Circle();
+        System.out.println("The area of the circle of radius "+circle1.radius+" is "+circle1.getArea());
+
+        //create a circle with radius 25
+        Circle circle2=new Circle(25);
+        System.out.println("The area of the circle of radius "+circle2.radius+" is "+circle2.getArea());
+
+        //create a circle with radius 125
+        Circle circle3=new Circle(125);
+        System.out.println("The area of the circle of radius "+circle3.radius+" is "+circle3.getArea());
+
+        //modify circle radius
+        circle2.radius=100;
+        System.out.println("The area of the circle of radius "+circle2.radius+" is "+circle2.getArea());
+    }
+}
+
+//Define the circle class
+class Circle{
+    double radius;
+    /**construct circle with radius 1*/
+    Circle(){
+        radius=1;
+    }
+    /**construct circle with a specified radius*/
+    Circle(double newRadius){
+        radius=newRadius;
+    }
+
+    /**return the area of this circle*/
+    double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+    /**return the perimeter of this circle*/
+    double getPerimeter(){
+        return 2*radius*Math.PI;
+    }
+
+    /**set a new radius for this circle*/
+    void setRadius(double newRadius){
+        radius=newRadius;
+    }
+}
