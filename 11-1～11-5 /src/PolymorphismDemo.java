7fd82250dac03e48c8e54c04e009bacd4f7c29bd
yuding <453594138@qq.com>
2019-11-05 16:21:24 +0800

    11-1～11-10
    new files

---
+++ 11-1～11-5 /src/PolymorphismDemo.java
@@ -0,0 +1,12 @@
+public class PolymorphismDemo {
+    /**main method */
+    public static void main(String[] args){
+        //Display circle and rectangle properties
+        displayObject(new Circle(1,"red",false));
+        displayObject(new Rectangle(1,1,"black",true));
+    }
+    /** Display geometric object properties */
+    public static void displayObject(GeometricObject object){
+        System.out.println("Created on "+object.getDateCreated()+". Color is "+object.getColor());
+    }
+}
